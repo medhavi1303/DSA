ccf149de37fb38c6a059478759bbff048e197230
medhavi1303 <medhavi.jain1303@gmail.com>
2026-06-22 11:35:47 +0530

    Time: 3 ms (12.33%) | Memory: 8.8 MB (84.03%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-        unordered_map<char,int> mp;
-        for(int i=0;i<text.size();i++){
-            if(text[i]=='b') mp[text[i]]++;
-            else if(text[i]=='a') mp[text[i]]++;
-            else if(text[i]=='l') mp[text[i]]++;
-            else if(text[i]=='o') mp[text[i]]++;
-            else if(text[i]=='n') mp[text[i]]++;
+        int b = 0, a = 0, l = 0, o = 0, n = 0;
+
+        for (char c : text) {
+            if (c == 'b') b++;
+            else if (c == 'a') a++;
+            else if (c == 'l') l++;
+            else if (c == 'o') o++;
+            else if (c == 'n') n++;
         }
 
-        int min=1000000;
-        if(mp['b']/1<min) min=mp['b']/1;
-        if(mp['a']/1<min) min=mp['a']/1;
-        if(mp['l']/2<min) min=mp['l']/2;
-        if(mp['o']/2<min) min=mp['o']/2;
-        if(mp['n']/1<min) min=mp['n']/1;
-        return min;
+        return min({b, a, l / 2, o / 2, n});
     }
 };
